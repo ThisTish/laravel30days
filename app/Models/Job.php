f5d51cf7c1c45eecdd5dd73bd2dd338461ac71a1
Tish Sirface <tish.sirface@gmail.com>
2025-10-30 02:52:49 -0600

    Eloquent day9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,43 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => '1',
-                'title' => 'Director',
-                'salary' => '$50, 000'
-            ],
-            [
-                'id' => '2',
+    protected $table = 'job_listings';
 
-                'title' => 'Teacher',
-                'salary' => '$25, 000'
-            ],
-            [
-                'id' => '3',
-
-                'title' => 'supervisor',
-                'salary' => '$10 000'
-            ]
-        ];
-    }
-
-
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => (int)$job['id'] === $id);
-
-        if(!$job) {
-            abort(404);
-        }
-        
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
 
 
