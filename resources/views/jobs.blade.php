b1f9e795d7970b80e1d9ae8e3e1e80af96263aa7
Tish Sirface <tish.sirface@gmail.com>
2025-11-03 03:46:24 -0700

    eager loading & pagination day 13 & 14

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,16 +1,23 @@
 <x-layout>
     <x-slot:heading class="text-white">
-        Job Listing
+        Job Listings
     </x-slot:heading>
-        <div class="bg-white border border-black size-50 text-xl">
-        @foreach($jobs as $job)
+        <div >
 
-    <a href="/jobs/{{ $job['id']}}"> {{ $job['title']}}: pays {{$job['salary']}}!  </a><br>
+        @foreach($jobs as $job)
+        
+    <a href="/jobs/{{ $job['id']}}" class="block space-y-2 mx-10 my-5 px-4 py-6 border border-gray-300 rounded-2xl w-48 h-fit" > 
+        <h2 class="font-bold">{{ $job['title'] }} </h2>
+        <p class="italic"> {{ $job->employer->name}} </p>
+        <p class="mt-2 text-end"> {{ $job['salary']}} </p>
+</a>
 
     @endforeach
 
     </div>
-
+<div>
+    {{ $jobs->links() }} 
+    </div>
 
     
 </x-layout>
